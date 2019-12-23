a9fccc771d69fdbc98b488cb784d48d6f5bd20f9
Iqbal Syamil Ayasy <iqbalsyamilayas@gmail.com>
2019-12-23 14:22:58 +0700

    send output as YAML file & add new headline (The Editorial and The Headlines)

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const cheerio = require('cheerio');
 const fetch = require('node-fetch');
-
+const fs = require('fs');
+const yaml = require('js-yaml');
 
 const kubeweekly = async () => {
     try {
@@ -10,42 +11,57 @@ const kubeweekly = async () => {
         return error;
     }
 }
-
-const kubeWeeklyParser = (htmlRaw) => {
+const kubeWeeklyContentTitle = async (htmlRaw) => {
+    const $ = cheerio.load(htmlRaw);
+    
+    return $('#templateHeader').text()
+}
+const kubeWeeklyParser = (htmlRaw,contentHeadline) => {
     const regexElement = /<a href=(.*?) [^>]*>(.*?)<\/a>/g;
-    const regexContent = /<a href=(.*?) [^>]*>(.*?)<\/a>/;
+    const regexContent = /<a href="(.*?)" [^>]*>(.*?)<\/a>/;
 
     let result = [];
     const $ = cheerio.load(htmlRaw);
     
-    result.push({headline:$('#templateHeader').text()})
-
     let data = $('table.mcnTextBlock > tbody.mcnTextBlockOuter')
         data.toArray().map(el => {
             let content = $.html(el)
-            if(content.includes("The Technical")){
-               content.match(regexElement).map(e => {
-                let parseData = regexContent.exec(e);
-                    result.push({
-                        title:parseData[2],
-                        link:parseData[1]
-                    })
-               })
+                if(content.includes(contentHeadline)){
+                   content.match(regexElement).map(e => {
+                    let parseData = regexContent.exec(e);
+                        result.push({
+                            title:parseData[2].trim(),
+                            link:parseData[1].trim()
+                        })
+                   })
             }
-    })
+        })
+    
     return result
 } 
 
 const main = async () => {
     try {
+        let result = []
+        const headlines =['The Technical','The Editorial','The Headlines']
         const kubeWeeklyContent = await kubeweekly();
-        const data = kubeWeeklyParser(kubeWeeklyContent)
-        console.log(data)
+        const kubeWeeklyContentHeadline = await kubeWeeklyContentTitle(kubeWeeklyContent);
+        result.push({title:kubeWeeklyContentHeadline})
+        headlines.map((contentHeadline) => {
+            const kubeweeklyData = kubeWeeklyParser(kubeWeeklyContent,contentHeadline)
+            result.push({
+                headline:contentHeadline,
+                data:kubeweeklyData
+            })
+        })
+        return result
     } catch (error) {
-        console.log(error)
+        return error;
     }
 }
 
-(async function() {
-    await main();
-})();
+main().then(data => {
+    let kubeweeklyYAML = yaml.safeDump(data);
+    fs.writeFileSync('kubeweekly.yaml', kubeweeklyYAML, 'utf8');
+    console.log('kubeweekly.yaml updated')
+})
