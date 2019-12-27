31a134413dca71a508ba7d47110cdb35dd6a60c0
Iqbal Syamil Ayasy <iqbalsyamilayas@gmail.com>
2019-12-27 17:12:25 +0700

    rollback to 5425fe3 , improve code style & add git module

--- index.js
+++ index.js
@@ -1,54 +1,54 @@
-const cheerio = require('cheerio');
-const fetch = require('node-fetch');
-const fs = require('fs');
-const yaml = require('js-yaml');
-
+const cheerio = require('cheerio')
+const fetch = require('node-fetch')
+const fs = require('fs')
+const yaml = require('js-yaml')
+const git = require('./git')
 const kubeweekly = async () => {
     try {
         const data = await fetch('https://kubeweekly.io')
         return data.text()
     } catch (error) {
-        return error;
+        return error
     }
 }
 
 const kubeWeeklyContentTitle = (htmlRaw) => {
-    const $ = cheerio.load(htmlRaw);
+    const $ = cheerio.load(htmlRaw)
     
     return $('#templateHeader').text()
 }
 const contentListTitle = (title) => {
     const regexPatternTitle = /kubeweekly(.*)#\d+/gi
-    const titleContent = title.match(regexPatternTitle)[0].toLowerCase() || null;
+    const titleContent = title.match(regexPatternTitle)[0].toLowerCase() || null
     const yamlFileName = titleContent.replace('#','').split(' ').join('')
-    return titleContent;
+    return titleContent
 }
 const yamlFileName = (title) => {
     const yamlName = title.replace('#','').split(' ').join('')
-    return yamlName;
+    return yamlName
 }
 const getDateTitle = (title) => {
     const regexPatternTitle = /\w+\s\d+,\s\d+/si
-    const dateParse = title.match(regexPatternTitle)
-    const date = new Date(Date.parse(dateParse)).toLocaleString().slice(0,10)
-    return date;
+    const dateParsed = title.match(regexPatternTitle)
+    const date = new Date(Date.parse(dateParsed)).toLocaleString().slice(0,10)
+    return date
 }
 
 const kubeWeeklyContentParser = (htmlRaw,contentHeadline) => {
-    const regexElement = /<a href=(.*?) [^>]*>(.*?)<\/a>/g;
-    const regexContent = /<a href="(.*?)" [^>]*>(.*?)<\/a>/;
+    const regexElement = /<a href=(.*?) [^>]*>(.*?)<\/a>/g
+    const regexContent = /<a href="(.*?)" [^>]*>(.*?)<\/a>/
 
-    let result = [];
-    const $ = cheerio.load(htmlRaw);
+    let result = []
+    const $ = cheerio.load(htmlRaw)
     
     let data = $('table.mcnTextBlock > tbody.mcnTextBlockOuter')
         data.toArray().map(el => {
             let content = $.html(el)
                 if(content.includes(contentHeadline)){
                    content.match(regexElement).map(e => {
-                    let parseData = regexContent.exec(e);
-                    const contentLink = parseData[1].replace(/\n/g, " ").trim()
-                    const contentTitle = parseData[2].replace(/\n/g, " ").trim()
+                    let parsedData = regexContent.exec(e)
+                    const contentLink = parsedData[1].replace(/\n/g, " ").trim()
+                    const contentTitle = parsedData[2].replace(/\n/g, " ").trim()
                     const contentType = contentHeadline.split(' ')[1].toLowerCase()
                         result.push({
                             title:contentTitle,
@@ -67,7 +67,7 @@ const main = async () => {
         let result = []
         let resultKubeweeklyContent = []
         const headlines =['The Technical','The Editorial','The Headlines']
-        const kubeWeeklyContent = await kubeweekly();
+        const kubeWeeklyContent = await kubeweekly()
         const kubeWeeklyContentHeadlineRaw = kubeWeeklyContentTitle(kubeWeeklyContent)
         const dateTitle = getDateTitle(kubeWeeklyContentHeadlineRaw)
         const contentTitle = contentListTitle(kubeWeeklyContentHeadlineRaw)
@@ -85,20 +85,21 @@ const main = async () => {
         result.push({data:resultKubeweeklyContent})
         return result
     } catch (error) {
-        return error;
+        return error
     }
 }
 
 main().then(data => {
-    let existingContentYaml = yaml.safeLoad(fs.readFileSync('./contentList.yaml','utf8'));
+    git.pull()
+    let existingContentYaml = yaml.safeLoad(fs.readFileSync('./contentList.yaml','utf8'))
     let headerContent = data.shift()
-    const found = existingContentYaml.contentList.some(el => el.date === headerContent.date);
+    const found = existingContentYaml.contentList.some(el => el.date === headerContent.date)
     if(!found){
         let content = {
             ...headerContent,
             ...data[0]
         }
-        const yamlName = './contents/'+yamlFileName(headerContent.title)+'.yaml';
+        const yamlName = './contents/'+yamlFileName(headerContent.title)+'.yaml'
         existingContentYaml.contentList.push({
             title: headerContent.title,
             tags: ["#kubereads"],
@@ -106,10 +107,11 @@ main().then(data => {
             status: 'not delivered',
             content: yamlName
         })
-        let kubeweeklyContentYAML = yaml.safeDump(content);
-        let kubeweeklyContentListYAML = yaml.safeDump(existingContentYaml);
-        fs.writeFileSync(yamlName, kubeweeklyContentYAML, 'utf8');
+        let kubeweeklyContentYAML = yaml.safeDump(content)
+        let kubeweeklyContentListYAML = yaml.safeDump(existingContentYaml)
+        fs.writeFileSync(yamlName, kubeweeklyContentYAML, 'utf8')
         fs.writeFileSync('./contentList.yaml',kubeweeklyContentListYAML,'utf8')
+        git.push()
         console.log('kubeweekly.yaml updated')
     }else console.log('kubeweekly not updated')
 })
